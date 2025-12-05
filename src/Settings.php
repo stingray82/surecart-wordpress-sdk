ebfdee1d96a658fcfe88b937198964aae217fa2d
Stingray82 <11146912+stingray82@users.noreply.github.com>
2025-12-05 23:58:41 +0000

    add an __isset() magic method to the SureCart\Licensing\Settings class to complement the existing __get() / __set() implementation.

--- src/Settings.php
+++ src/Settings.php
@@ -441,4 +441,17 @@ class Settings {
 	public function __set( $name, $value ) {
 		return $this->set_option( 'sc_' . $name, $value );
 	}
+
+	/**
+	 * isset handler so empty()/isset() work with __get().
+	 *
+	 * @param string $name Option name.
+	 *
+	 * @return bool
+	 */
+	public function __isset( $name ) {
+		$options = $this->get_options();
+		return isset( $options[ 'sc_' . $name ] );
+	}
 }
+
